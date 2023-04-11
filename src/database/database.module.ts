bbfde4566ef7e02ae1bad1ffb4652c9252d96f1d
komosyu4649 <komosyu4649@gmail.com>
2023-04-11 22:37:18 +0900

    wip: database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseController } from './database.controller';
 import { DatabaseService } from './database.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [DatabaseController],
   providers: [DatabaseService],
 })
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -3,7 +3,16 @@ import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class DatabaseService {
-  async findAll(): Promise<string> {
-    return 'This action returns all database service';
+  constructor(private httpService: HttpService) {}
+  async findAll(): Promise<any> {
+    // const result = await this.httpService.get(
+    //   'https://seeshanchu.cdn.newt.so/v1',
+    // );
+    // return result;
+    // return 'test';
+    const test = await this.httpService.get(
+      'https://jsonplaceholder.typicode.com/todos/1',
+    );
+    return test;
   }
 }
